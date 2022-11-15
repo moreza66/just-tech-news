1c3008ae55d100c4f3ab8fa46bfc575c4fa4de33
mohammadreza memar <mohammadrezamemar@mohammarezasMBP.attlocal.net>
2022-11-15 11:50:54 -0600

    practicing

---
+++ server.js
@@ -0,0 +1,9 @@
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+const routes = require('./controllers/');
